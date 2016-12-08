25abcc9351514b7b2dcb543feb3101df090473cc
Robert Jackson <rjackson@linkedin.com>
2016-12-08 15:12:41 -0500

    Remove options manipulation.

--- index.js
+++ index.js
@@ -96,29 +96,6 @@ module.exports = {
     return (this.parent && this.parent.options) || (this.app && this.app.options) || {};
   },
 
-  _shouldCompileModules: function(addonOptions) {
-    var babelOptions = addonOptions.babel;
-    var customOptions = addonOptions['ember-cli-babel'];
-
-    if (this._shouldShowBabelDeprecations && !this._modulesDeprecationPrinted &&
-      babelOptions && 'compileModules' in babelOptions) {
-
-      this._modulesDeprecationPrinted = true;
-
-      // we can use writeDeprecateLine() here because the warning will only be shown on newer Ember CLIs
-      this.ui.writeDeprecateLine(
-        'Putting the "compileModules" option in "babel" is deprecated, please put it in "ember-cli-babel" instead.');
-    }
-
-    if (customOptions && 'compileModules' in customOptions) {
-      return customOptions.compileModules === true;
-    } else if (babelOptions && 'compileModules' in babelOptions) {
-      return babelOptions.compileModules === true;
-    } else {
-      return false;
-    }
-  },
-
   _getBabelOptions: function() {
     var parentName;
 
@@ -133,66 +110,6 @@ module.exports = {
     var addonOptions = this._getAddonOptions();
     var options = clone(addonOptions.babel || {});
 
-    var compileModules = this._shouldCompileModules(addonOptions);
-
-    var ui = this.ui;
-
-    options.annotation = 'Babel: ' + parentName;
-    // pass a console object that wraps the addon's `UI` object
-    options.console = {
-      log: function(message) {
-        // fallback needed for support of ember-cli < 2.2.0
-        if (ui.writeInfoLine) {
-          ui.writeInfoLine(message);
-        } else {
-          ui.writeLine(message, 'INFO');
-        }
-      },
-
-      warn: function(message) {
-        // fallback needed for support of ember-cli < 2.2.0
-        if (ui.writeWarnLine) {
-          ui.writeWarnLine(message);
-        } else {
-          ui.writeLine(message, 'WARN');
-        }
-      },
-
-      error: function(message) {
-        // fallback needed for support of ember-cli < 2.2.0
-        if (ui.writeError) {
-          ui.writeError(message);
-        } else {
-          ui.writeLine(message, 'ERROR');
-        }
-      }
-    };
-
-    // Ensure modules aren't compiled unless explicitly set to compile
-    options.blacklist = options.blacklist || ['es6.modules'];
-
-    // do not enable non-standard transforms
-    if (!('nonStandard' in options)) {
-      options.nonStandard = false;
-    }
-
-    // Remove custom options from `options` hash that is passed to Babel
-    delete options.includePolyfill;
-    delete options.compileModules;
-
-    var blacklistModulesIndex = options.blacklist.indexOf('es6.modules');
-    if (compileModules) {
-      if (blacklistModulesIndex >= 0) {
-        options.blacklist.splice(blacklistModulesIndex, 1);
-      }
-    } else {
-      if (blacklistModulesIndex < 0) {
-        options.blacklist.push('es6.modules');
-      }
-    }
-
-    // Ember-CLI inserts its own 'use strict' directive
-    options.blacklist.push('useStrict');
     options.highlightCode = false;
 
     return options;
