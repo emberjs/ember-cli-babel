21b86a88ce8c10f1241d030dc95421bed0411ac9
Chris Manson <chris@manson.ie>
2026-02-01 22:04:06 +0000

    update workerpool name

--- node-tests/utils/terminate-workers.js
+++ node-tests/utils/terminate-workers.js
@@ -5,7 +5,7 @@ let ParallelApi = require('broccoli-babel-transpiler/lib/parallel-api');
 module.exports = function terminateWorkerPool() {
   // shut down any workerpool that is running at this point
   let babelCoreVersion = ParallelApi.getBabelVersion();
-  let workerPoolId = 'v2/broccoli-babel-transpiler/workerpool/babel-core-' + babelCoreVersion;
+  let workerPoolId = 'v2/broccoli-babel-transpiler/async-workerpool/babel-core-' + babelCoreVersion;
   let runningPool = process[workerPoolId];
 
   if (runningPool) {
